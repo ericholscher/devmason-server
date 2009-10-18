90991af1b5c50c106bf18b743c9e29e9aafd5f32
Eric Holscher <eric@ericholscher.com>
2009-10-18 18:49:47 -0500

    Remove package namespace in the URLS since it was useless.

--- pony_server/api/urls.py
+++ pony_server/api/urls.py
@@ -8,14 +8,14 @@ build_handler = Resource(BuildHandler)
 tag_handler = Resource(TagHandler)
 
 urlpatterns = patterns('',
-   url(r'^package/(?P<slug>[^/]+)/builds/latest/', build_handler, {'latest': True }),
-   url(r'^package/(?P<slug>[^/]+)/builds/(?P<data>[^/]+)/', build_handler),
-   url(r'^package/(?P<slug>[^/]+)/builds/', build_handler),
+   url(r'^(?P<slug>[^/]+)/builds/latest/', build_handler, {'latest': True }),
+   url(r'^(?P<slug>[^/]+)/builds/(?P<data>[^/]+)/', build_handler),
+   url(r'^(?P<slug>[^/]+)/builds/', build_handler),
 
-   url(r'^package/(?P<slug>[^/]+)/tags/(?P<data>[^/]+)/latest/', tag_handler, {'latest': True }),
-   url(r'^package/(?P<slug>[^/]+)/tags/(?P<data>[^/]+)/', tag_handler),
-   url(r'^package/(?P<slug>[^/]+)/tags/', tag_handler),
+   url(r'^(?P<slug>[^/]+)/tags/(?P<data>[^/]+)/latest/', tag_handler, {'latest': True }),
+   url(r'^(?P<slug>[^/]+)/tags/(?P<data>[^/]+)/', tag_handler),
+   url(r'^(?P<slug>[^/]+)/tags/', tag_handler),
 
-   url(r'^package/(?P<slug>[^/]+)/', package_handler),
+   url(r'^(?P<slug>[^/]+)/', package_handler),
    url(r'^$', root_handler),
 )
