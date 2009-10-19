f152f5142c0b8b17386ca2209091654d6c4ef899
Eric Holscher <eric@ericholscher.com>
2009-10-19 03:21:51 -0500

    Add tags and packages to the admin

--- pony_server/admin.py
+++ pony_server/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from pony_server.models import Result, Client
+from pony_server.models import Result, Client, Package, Tag
 
 
 admin.site.register(Client)
 admin.site.register(Result)
-
+admin.site.register(Package)
+admin.site.register(Tag)
