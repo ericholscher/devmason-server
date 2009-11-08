613fe3d82df2303f660ae1b7b68129e477bd4f4c
Eric Holscher <eric@ericholscher.com>
2009-11-08 16:38:24 -0600

    Fancier view.

--- pony_server/views.py
+++ pony_server/views.py
@@ -1,6 +1,23 @@
+try:
+    import json
+except:
+    import simplejson as json
+
+import datetime
+from pony_server.models import Repository, BuildRequest, Project
+
 def start_build(request):
-       import ipdb; ipdb.set_trace()
-       obj = simplejson.loads(request.POST['payload'])
-       url = obj['repository']['url']
-       hash = obj['after']
-       git_url = url.replace('http://', 'git://')
+    obj = json.loads(request.POST['payload'])
+    name = obj['repository']['name']
+    url = obj['repository']['url']
+    git_url = url.replace('http://', 'git://')
+    hash = obj['after']
+
+    import ipdb; ipdb.set_trace()
+    project = Project.objects.get(slug=name)
+    repo = Repository.objects.get_or_create(url=git_url)
+    brequest = BuildRequest.objects.create(
+        repository = repo,
+        identifier = hash,
+        requested = datetime.datetime.utcnow(),
+    )
