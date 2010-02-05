7dce0f6a2a877d7b0583526ec52be7ec8cf00d4d
Eric Holscher <eric@ericholscher.com>
2010-02-05 11:52:49 +0000

    Don't blow up if IRC is gone.

--- pony_server/signals.py
+++ pony_server/signals.py
@@ -9,9 +9,12 @@ srv = "irc.freenode.net"
 chan = "#devmason"
 
 def send_irc_msg(msg):
-    t = telnetlib.Telnet()
-    t.open('10.177.22.217', port=13337)
-    t.write('%s %s&%s&%s\n' % (pw, srv, chan, msg))
+    try:
+        t = telnetlib.Telnet()
+        t.open('10.177.22.217', port=13337)
+        t.write('%s %s&%s&%s\n' % (pw, srv, chan, msg))
+    except:
+        pass
 
 def irc_handler(sender, **kwargs):
     build = kwargs['instance']
