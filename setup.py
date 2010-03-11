c61ef89d7ebf03a243540189fecd87de4a9501d5
Eric Holscher <eric@ericholscher.com>
2010-03-10 20:12:35 -0600

    Actually rename the source directory and related files.

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
-setup(name='pony_server',
+setup(name='devmason_server',
       version='1.0pre-20091019',
       description='A Django implementation of a the pony-build server.',
       author = 'Eric Holscher, Jacob Kaplan-Moss',
-      url = 'http://github.com/jacobian/pony_server',
+      url = 'http://github.com/ericholscher/devmason-server',
       license = 'BSD',
-      packages = ['pony_server'],
+      packages = ['devmason_server'],
       install_requires=['django-tagging>=0.3',
                         'django-piston>=0.2.2',
                         'mimeparse>=0.1.2'],
