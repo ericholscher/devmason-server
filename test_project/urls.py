f1260a230fea535aded6a36c67ab1c045706d63f
Eric Holscher <eric@ericholscher.com>
2010-08-06 23:12:10 -0500

    Added a test_project for easy evaluation.

---
+++ test_project/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls.defaults import *
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    (r'', include('devmason_server.urls')),
+    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/(.*)', admin.site.root, name='admin-root'),
+
+)
